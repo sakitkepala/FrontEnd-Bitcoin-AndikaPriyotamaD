0dfcfe365815cabe363e81522b821f36ed8ebca7
sakitkepala <andikapriyotama@gmail.com>
2021-05-20 02:34:25 +0700

    Percobaan pertama komponen Page `Monitor`

--- resources/js/Pages/Monitor.js
+++ resources/js/Pages/Monitor.js
@@ -1,5 +1,34 @@
 import * as React from "react";
 
+const MATA_UANG = ["AUD", "EUR", "GBP", "JPY", "USD"];
+
+function filterDataKurs(data, daftarMataUang) {
+    return daftarMataUang.reduce(
+        (nilai, simbol) => ({ ...nilai, [simbol]: data[simbol] }),
+        {}
+    );
+}
+
 export default function Monitor() {
-    return <div>Monitoring!</div>;
+    const [dataKurs, setDataKurs] = React.useState(null);
+
+    React.useEffect(() => {
+        fetch("https://blockchain.info/ticker").then(async respon => {
+            const data = await respon.json();
+
+            if (!respon.ok) {
+                return Promise.reject(error => error);
+            }
+
+            const listHarga = filterDataKurs(data, MATA_UANG);
+            setDataKurs(listHarga);
+        });
+    }, []);
+
+    return (
+        <div>
+            {dataKurs &&
+                `Jual: ${dataKurs.USD.sell}, Beli: ${dataKurs.USD.buy}`}
+        </div>
+    );
 }
