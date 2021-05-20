8c26d54b97d6407d93a86c837cb67ea052202529
sakitkepala <andikapriyotama@gmail.com>
2021-05-20 16:56:17 +0700

    Styling link navigasi

--- resources/js/komponen/layout.js
+++ resources/js/komponen/layout.js
@@ -5,9 +5,15 @@ function Layout({ children }) {
         <div className="screen">
             <header className="header">
                 <nav className="navigasi">
-                    <a href="/">Depan</a>
-                    <a href="/btc-ke-idr">BTC&rarr;IDR</a>
-                    <a href="/idr-ke-btc">IDR&rarr;BTC</a>
+                    <div className="item-link">
+                        <a href="/">Depan</a>
+                    </div>
+                    <div className="item-link">
+                        <a href="/btc-ke-idr">BTC&rarr;IDR</a>
+                    </div>
+                    <div className="item-link">
+                        <a href="/idr-ke-btc">IDR&rarr;BTC</a>
+                    </div>
                 </nav>
             </header>
 
